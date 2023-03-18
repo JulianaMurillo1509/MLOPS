dd39bc1352047057bfcb48b50749c1718f92d40b
leonardo.talero <leonardo.talero@kinesso.com>
2023-03-18 16:59:19 -0500

    text, test text table

--- docker-compose/simple_train/Routers/trainModel.py
+++ docker-compose/simple_train/Routers/trainModel.py
@@ -138,6 +138,7 @@ def read_data(data):
     penguins = pd.DataFrame(result.fetchall(), columns=result.keys())
     print('***penguins data:***', penguins.head())
     for penguin in penguins[:-2]:
+        print("***penguin***:",penguin)
         print(penguin.species, penguin.island, penguin.bill_length_mm, penguin.bill_depth_mm, penguin.flipper_length_mm,
               penguin.body_mass_g, penguin.sex,penguin.year)
     session.close()
