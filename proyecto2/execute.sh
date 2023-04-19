0d89ef3c3d5c86bda2556f89d8ebbe9510da4f2d
leonardo.talero <leonardo.talero@kinesso.com>
2023-04-19 13:17:38 -0500

    readme.md and fix sh  required

--- proyecto2/execute.sh
+++ proyecto2/execute.sh
@@ -33,6 +33,8 @@ if microk8s status | grep -q "microk8s is running"; then
     echo "Checking if MicroK8s kubectl get nodes are ready.."
     # Check if Kubernetes API server is ready
     sleep 90s
+    # execute compose to generate yaml
+    chmod u+w komposefiles/
     kompose convert -f docker-compose.yml -o komposefiles/ --volumes hostPath
     if ! microk8s kubectl get pods | grep -q  "Running"; then
       echo "Kubernetes API server is not ready."
